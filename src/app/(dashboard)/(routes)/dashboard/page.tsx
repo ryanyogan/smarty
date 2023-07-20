e079c382744c049dbec64fe93464da85c39a7220
Ryan Yogan <ryanyogan@gmail.com>
2023-07-20 13:05:00 -0500

    sidebar wip // desktop only

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,5 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <h1>Dashboard Page (Authenticated)</h1>
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <div className="flex justify-center items-center h-full">dashboard</div>
   );
 }
