4ceabc9049a6427128e7eb61de977417ba5719f0
Ryan Yogan <ryanyogan@gmail.com>
2023-07-23 22:22:43 -0500

    work in progress // music

---
+++ src/app/(dashboard)/(routes)/music/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import Empty from "@/components/shared/empty";
+import Heading from "@/components/shared/heading";
+import Loader from "@/components/shared/loader";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Music } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { formSchema } from "./constants";
+
+export default function MusicPage() {
+  const router = useRouter();
+  const [music, setMusic] = useState<string | undefined>("");
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      prompt: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setMusic(undefined);
+
+      const response = await axios.post("/api/music", values);
+
+      setMusic(response.data.audio);
+
+      form.reset();
+    } catch (error) {
+      // TODO: Open Modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Music Generation"
+        description="Make a funky beat!"
+        icon={Music}
+        iconColor="text-emerald-500"
+        bgColor="bg-emerald-500/10"
+      />
+
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        disabled={isLoading}
+                        placeholder="Piano solo"
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                variant="default"
+                disabled={isLoading}
+                className="col-span-12 lg:col-span-2 w-full"
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {!music && !isLoading && <Empty label="No music generated" />}
+
+          {music && (
+            <audio controls className="w-full mt-8">
+              <source src={music} />
+            </audio>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
